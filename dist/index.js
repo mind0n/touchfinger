7f0cb4c748d5ecfb2377a2d1ee3fc3cd37421b0d
mind0n <markyuanmj@126.com>
2020-09-26 23:08:37 +0800

    publish

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Sample exports from other modules
+var tnnd = require("tnnd");
+// Sample export of our own
+var a = new tnnd.Bar();
+//# sourceMappingURL=index.js.map
---
+++ ts/index.ts
@@ -0,0 +1,5 @@
+// Sample exports from other modules
+import * as tnnd from "tnnd";
+
+// Sample export of our own
+var a = new tnnd.Bar();
